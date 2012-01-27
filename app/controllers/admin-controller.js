4009a91f500c21ecb0d63ea7cf869950c6170aab
David McClure <david.mcclure@virginia.edu>
2012-01-27 10:23:55 -0500

    shell for admin controller

---
+++ app/controllers/admin-controller.js
@@ -0,0 +1,53 @@
+/*
+ * Admin controller
+ *
+ * @package     exquisite-haiku
+ * @author      David McClure
+ * @license     Apache 2.0
+ */
+
+// Module dependencies.
+var forms = require('../../helpers/forms')
+
+// Models.
+// var User = mongoose.model('User');
+
+// Controller actions.
+module.exports = function(app) {
+
+    /*
+     * GET /admin
+     */
+    app.get('/admin', function(req, res) {
+        res.send('GET /admin');
+    });
+
+    /*
+     * GET /new
+     */
+    app.get('/new', function(req, res) {
+        res.send('GET /new');
+    });
+
+    /*
+     * POST /register
+     */
+    app.post('/new', function(req, res) {
+        res.send('POST /new');
+    });
+
+    /*
+     * POST /start
+     */
+    app.post('/start', function(req, res) {
+        res.send('POST /start');
+    });
+
+    /*
+     * POST /stop
+     */
+    app.post('/stop', function(req, res) {
+        res.send('POST /stop');
+    });
+
+}
